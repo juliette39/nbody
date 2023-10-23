4030f049a659b3122f6f557c4be6a112ca1608eb
juliette.debono <juliette.debono@telecom-sudparis.eu>
2023-10-23 16:07:51 +0200

    début openMP

--- sequential/produceStatisticsOMP.py
+++ sequential/produceStatisticsOMP.py
@@ -2,28 +2,32 @@ import os
 import subprocess
 import matplotlib.pyplot as plt
 
+FILEPATH = os.path.dirname(os.path.abspath(__file__))
 
 max_cpu = os.cpu_count()
 data = []
 
-nb_particles = 5000
-T_FINAL = 5
+nb_particles = 2000
+T_FINAL = 3
 
-for i in range(1, max_cpu + 1) :
-    print(f'Calculate for {i} core(s)')
-    os.environ['OMP_NUM_THREADS'] = str(i)
-    result = subprocess.run(['./nbody_barnes_hut', f'{nb_particles} {T_FINAL}'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+for i in range(1, max_cpu + 1):
+    if i > 1:
+        print(f'Calculate for {i} cores')
+    else:
+        print(f'Calculate for {i} core')
+
+    result = subprocess.run([f'{FILEPATH}/nbody_brute_force', nb_particles, T_FINAL, i], stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
     output = result.stdout.decode('utf-8')
+    print(output)
 
     lines = output.strip().split('\n')
     last_line = lines[-1]
     parts = last_line.split()
     if len(parts) > 0:
         time_taken = parts[2]
-
-    time_taken = float(time_taken)
-    data.append(time_taken)
-
+        time_taken = float(time_taken)
+        data.append(time_taken)
 
 x = list(range(1, max_cpu + 1))
 
